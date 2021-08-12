9b05272cab1688232849520cab5abb205423b75f
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-08-11 22:33:36 -0400

    setup item and attack lists

--- src/backend/items.js
+++ src/backend/items.js
@@ -1,20 +1,24 @@
 class Item {
   constructor(
+    owner,
     name,
     description,
     value,
     hidediff,
     weight,
-    attacks, // array
     ranged,
+    armor,
+    dexpenalty,
   ) {
+    this.owner = owner;
     this.name = name;
     this.description = description;
     this.value = value;
     this.hidediff = hidediff;
     this.weight = weight;
-    this.attacks = attacks;
     this.ranged = ranged;
+    this.armor = armor;
+    this.dexpenalty = dexpenalty;
   }
 }
 
